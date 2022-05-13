c891019a5e61ae99cb10e9187c9d6a0d43967c18
kkksb <kkex5027@gmail.com>
2022-05-13 22:05:25 +0900

    add: 受付処理を修正

--- README.md
+++ README.md
@@ -3,12 +3,5 @@
 以下のような簡単なCLIアプリを作る。
 
 ```
-$ pybank show
-10000円
-
-$ pybank deposit 10000円
-10000円を追加
-
-$ pybank withdrow 10000円
-10000円を引き出した
+$ python cli.py operate
 ```
--- pybank/libs/reception.py
+++ pybank/libs/reception.py
@@ -7,10 +7,14 @@ class Reception:
         
         while True:
             print(f"以下のメニューから操作を選択してください[1~{last_menu_number}]")
+            print("1: 残高確認 2: 預入れ 3: 引き出し 4: プログラムを終了")
             user_choice = int(input())
 
             if user_choice in opc.OperationManual.operation_numbers:
-                break
+                pass
+                if user_choice == last_menu_number:
+                    print("プログラムを終了します")
+                    exit()
             else:
                 print("メニューから選択してください")
         return opc.OperationManual.operation_map.get(user_choice)
